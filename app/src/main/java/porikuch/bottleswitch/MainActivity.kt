62ce53462161dcc92b28e9cd72755c038e0db436
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-10-02 22:02:39 +0900

    implements to display bottle list on main page

--- app/src/main/java/porikuch/bottleswitch/MainActivity.kt
+++ app/src/main/java/porikuch/bottleswitch/MainActivity.kt
@@ -3,22 +3,27 @@ package porikuch.bottleswitch
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                //message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                //message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                //message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,6 +33,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setSupportActionBar(toolbar)
+
+        layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recycler_view.adapter = adapter
+
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
