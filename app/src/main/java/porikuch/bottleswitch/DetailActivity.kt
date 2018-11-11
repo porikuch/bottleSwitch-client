38e221d1be28528ba58e6b06fc64a03ec3c6a580
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-11-11 22:17:45 +0900

    implements float bottle button event which requests server

--- app/src/main/java/porikuch/bottleswitch/DetailActivity.kt
+++ app/src/main/java/porikuch/bottleswitch/DetailActivity.kt
@@ -1,32 +1,82 @@
 package porikuch.bottleswitch
 
+import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.httpPost
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private var mFloatBottleTask: BottleFloatTask? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val bottleId: Int = intent.getIntExtra("Bottle_ID", 0)
+        val myId: Int = intent.getIntExtra("USER_ID", 0)
+        val createdUserId: Int = intent.getIntExtra("CREATED_ID", 0)
+
         // TODO request Bottle Data
         val title: String = "This is title"
         val body: String = "This is body.\nHello World."
         titleTextView.text = title
         bodyTextView.text = body
 
-        val myId: Int = intent.getIntExtra("USER_ID", 0)
-        val createdUserId: Int = intent.getIntExtra("CREATED_ID", 0)
         if (myId != 0 && myId == createdUserId) {
             floatButton.text = "Float Bottle"
             getSwitchButton.visibility = View.GONE
         } else {
             floatButton.text = "Float Again"
             getSwitchButton.setOnClickListener { _ ->
-
+                // TODO intent
             }
+        }
+
+        floatButton.setOnClickListener { _ ->
+            // TODO request server
+            mFloatBottleTask = BottleFloatTask(bottleId)
+            mFloatBottleTask!!.execute(null as Void?)
+
+        }
+    }
+
+    inner class BottleFloatTask internal constructor(private val bottleId: Int): AsyncTask<Void, Void, Boolean>() {
+        var status = false
+        override fun doInBackground(vararg param: Void): Boolean? {
+            val (request, response, result) = "http://$SERVER_ADDRESS/release_bottle".httpPost(listOf("id" to bottleId)).responseJson()
+            result.fold(success = { json ->
+                status = json.obj()["status"].toString().toBoolean()
+                if (status) {
+                    finish()
+                }
+            }, failure = { error ->
+                Log.e("error", error.toString())
+            })
+
+            return status
+        }
+
+        override fun onPostExecute(success: Boolean?) {
+            mFloatBottleTask = null
+            //showProgress(false)
+
+            if (success!!) {
+                finish()
+            } else {
+                //password.error = getString(R.string.error_incorrect_password)
+                //password.requestFocus()
+            }
+        }
+
+        override fun onCancelled() {
+            mFloatBottleTask = null
+            //showProgress(false)
         }
     }
 }
