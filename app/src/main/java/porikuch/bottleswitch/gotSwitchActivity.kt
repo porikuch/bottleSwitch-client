cb1b52d50425fbd45926776928bf3e47539150ac
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-11-12 17:46:14 +0900

    add get switch activity

---
+++ app/src/main/java/porikuch/bottleswitch/gotSwitchActivity.kt
@@ -0,0 +1,12 @@
+package porikuch.bottleswitch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class gotSwitchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_got_switch)
+    }
+}
